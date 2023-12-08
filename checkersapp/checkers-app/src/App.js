a87a6ee97adb1e76e0b98474f34c442ad029a488
cschraut <cschraut@villanova.edu>
2023-12-08 12:42:52 -0500

    Moving Pieces added

--- checkersapp/checkers-app/src/App.js
+++ checkersapp/checkers-app/src/App.js
@@ -3,103 +3,141 @@ import React from 'react';
 import {useState} from 'react';
 import red from './red-check.png';
 import white from './white-check.png';
+import blank from './empty-check.png';
 
 
 export default function Board() {
-  const [squares, setBoard] = useState(Array(64).fill(null));
+  const [squares, setBoard] = useState(
+    [null,red,null,red,null,red,null,red,
+    red,null,red,null,red,null,red,null,
+    null,red,null,red,null,red,null,red,
+    blank,null,blank,null,blank,null,blank,null,
+    null,blank,null,blank,null,blank,null,blank,
+    white,null,white,null,white,null,white,null,
+    null,white,null,white,null,white,null,white,
+    white,null,white,null,white,null,white,null
+  ]);
+  const [count, setCount] = useState(0);
+  
+  function handleClick(i){
+    const nextSquares = squares.slice()
+    if(count == 0){
+      nextSquares[i] = blank;
+      setBoard(nextSquares);
+      setCount(1)
+    }
+    if(count == 1){
+      nextSquares[i] = red
+      setBoard(nextSquares);
+      setCount(2)
+    }
+    if(count == 2){
+      nextSquares[i] = blank;
+      setBoard(nextSquares);
+      setCount(3)
+    }
+    if(count == 3){
+      nextSquares[i] = white;
+      setBoard(nextSquares);
+      setCount(0)
+    }
+    
+  }
   
   return (
     <div>
       <div className = "status"></div>
       <div className="board-row">
         <SquareWhite value = {squares[0]} />
-        <SquareBlack image = {red}/>
+        <SquareBlack image = {squares[1]} onSquareClick = {() => handleClick(1)}/>
         <SquareWhite value = {squares[2]} />
-        <SquareBlack image = {red} />
+        <SquareBlack image = {squares[3]} onSquareClick = {() => handleClick(3)}/>
         <SquareWhite value = {squares[4]} />
-        <SquareBlack image = {red} />
+        <SquareBlack image = {squares[5]} onSquareClick = {() => handleClick(5)}/>
         <SquareWhite value = {squares[6]} />
-        <SquareBlack image = {red} />
+        <SquareBlack image = {squares[7]} onSquareClick = {() => handleClick(7)}/>
       </div>
       <div className="board-row">
-        <SquareBlack image = {red} />
-        <SquareWhite value = {squares[9]} />
-        <SquareBlack image = {red} />
+        <SquareBlack image = {squares[8]}  onSquareClick = {() => handleClick(8)}/>
+        <SquareWhite value = {squares[9]}  />
+        <SquareBlack image = {squares[10]} onSquareClick = {() => handleClick(10)}/>
         <SquareWhite value = {squares[11]} />
-        <SquareBlack image = {red} />
+        <SquareBlack image = {squares[12]} onSquareClick = {() => handleClick(12)}/>
         <SquareWhite value = {squares[13]} />
-        <SquareBlack image = {red} />
+        <SquareBlack image = {squares[14]} onSquareClick = {() => handleClick(14)}/>
         <SquareWhite value = {squares[15]} />
       </div>
       <div className="board-row">
         <SquareWhite value = {squares[16]} />
-        <SquareBlack image = {red} />
+        <SquareBlack image = {squares[17]} onSquareClick = {() => handleClick(17)}/>
         <SquareWhite value = {squares[18]} />
-        <SquareBlack image = {red} />
+        <SquareBlack image = {squares[19]} onSquareClick = {() => handleClick(19)}/>
         <SquareWhite value = {squares[20]} />
-        <SquareBlack image = {red} />
+        <SquareBlack image = {squares[21]} onSquareClick = {() => handleClick(21)}/>
         <SquareWhite value = {squares[22]} />
-        <SquareBlack image = {red} />
+        <SquareBlack image = {squares[23]} onSquareClick = {() => handleClick(23)}/>
       </div>
       <div className="board-row">
-        <SquareBlack value = {squares[24]} />
+        <SquareBlack image = {squares[24]} onSquareClick = {() => handleClick(24)}/>
         <SquareWhite value = {squares[25]} />
-        <SquareBlack value = {squares[26]} />
+        <SquareBlack image = {squares[26]} onSquareClick = {() => handleClick(26)}/>
         <SquareWhite value = {squares[27]} />
-        <SquareBlack value = {squares[28]} />
+        <SquareBlack image = {squares[28]} onSquareClick = {() => handleClick(28)}/>
         <SquareWhite value = {squares[29]} />
-        <SquareBlack value = {squares[30]} />
+        <SquareBlack image = {squares[30]} onSquareClick = {() => handleClick(30)}/>
         <SquareWhite value = {squares[31]} />
       </div>
       <div className="board-row">
         <SquareWhite value = {squares[32]} />
-        <SquareBlack value = {squares[33]} />
+        <SquareBlack image = {squares[33]} onSquareClick = {() => handleClick(33)}/>
         <SquareWhite value = {squares[34]} />
-        <SquareBlack value = {squares[35]} />
+        <SquareBlack image = {squares[35]} onSquareClick = {() => handleClick(35)}/>
         <SquareWhite value = {squares[36]} />
-        <SquareBlack value = {squares[37]} />
+        <SquareBlack image = {squares[37]} onSquareClick = {() => handleClick(37)}/>
         <SquareWhite value = {squares[38]} />
-        <SquareBlack value = {squares[39]} />
+        <SquareBlack image = {squares[39]} onSquareClick = {() => handleClick(39)}/>
       </div>
       <div className="board-row">
-        <SquareBlack image = {white} />
+        <SquareBlack image = {squares[40]} onSquareClick = {() => handleClick(40)}/>
         <SquareWhite value = {squares[41]} />
-        <SquareBlack image = {white} />
+        <SquareBlack image = {squares[42]} onSquareClick = {() => handleClick(42)}/>
         <SquareWhite value = {squares[43]} />
-        <SquareBlack image = {white} />
+        <SquareBlack image = {squares[44]} onSquareClick = {() => handleClick(44)}/>
         <SquareWhite value = {squares[45]} />
-        <SquareBlack image = {white} />
+        <SquareBlack image = {squares[46]} onSquareClick = {() => handleClick(46)}/>
         <SquareWhite value = {squares[47]} />
       </div>
       <div className="board-row">
         <SquareWhite value = {squares[48]} />
-        <SquareBlack image = {white} />
+        <SquareBlack image = {squares[49]} onSquareClick = {() => handleClick(49)}/>
         <SquareWhite value = {squares[50]} />
-        <SquareBlack image = {white} />
+        <SquareBlack image = {squares[51]} onSquareClick = {() => handleClick(51)}/>
         <SquareWhite value = {squares[52]} />
-        <SquareBlack image = {white} />
+        <SquareBlack image = {squares[53]} onSquareClick = {() => handleClick(53)}/>
         <SquareWhite value = {squares[54]} />
-        <SquareBlack image = {white} />
+        <SquareBlack image = {squares[55]} onSquareClick = {() => handleClick(55)}/>
       </div>
       <div className="board-row">
-        <SquareBlack image = {white} />
+        <SquareBlack image = {squares[56]} onSquareClick = {() => handleClick(56)}/>
         <SquareWhite value = {squares[57]} />
-        <SquareBlack image = {white} />
+        <SquareBlack image = {squares[58]} onSquareClick = {() => handleClick(58)}/>
         <SquareWhite value = {squares[59]} />
-        <SquareBlack image = {white} />
+        <SquareBlack image = {squares[60]} onSquareClick = {() => handleClick(60)}/>
         <SquareWhite value = {squares[61]} />
-        <SquareBlack image = {white} />
+        <SquareBlack image = {squares[62]} onSquareClick = {() => handleClick(62)}/>
         <SquareWhite value = {squares[63]} />
       </div>
     </div>
     );
 }
 
-function SquareBlack({image}) {
+function SquareBlack({image, onSquareClick}) {
+  if(image )
   return (
   <button 
     className="square1">
-      <img src={image}></img>
+      <img src= {image} alt="piece" onClick = {onSquareClick}></img>
+      
   </button>
   );
 }
